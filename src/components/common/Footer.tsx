a08efd0c3304d74018ac80e79d45243d81f52135
shailendra kumar <shailendraftp000@gmail.com>
2023-12-28 11:56:16 +0530

    added

--- src/components/common/Footer.tsx
+++ src/components/common/Footer.tsx
@@ -4,7 +4,7 @@ import { commonImages } from "../../data/common/images";
 
 const Footer: React.FC = () => {
   return (
-    <div className="bg-bgFooterGreen text-white  p-10">
+    <div className="bg-bgFooterGreen text-white  p-10 mt-10">
       <div className="flex flex-col justify-center gap-7 sm:flex-row">
         <div className="flex flex-col items-center w-full sm:items-start sm:w-1/6">
           <img
