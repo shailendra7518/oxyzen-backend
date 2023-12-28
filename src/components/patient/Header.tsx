6a3445cc857efca47a0aeed11d8071daba74d3f8
shailendra kumar <shailendraftp000@gmail.com>
2023-12-28 10:35:16 +0530

    added

--- src/components/patient/Header.tsx
+++ src/components/patient/Header.tsx
@@ -13,24 +13,24 @@ import { IoLocationOutline } from "react-icons/io5";
 
 // Add the styled header section code here
 export const StyledHeader = () => (
-  <header className="bg-white  p-4 relative z-10 ">
-    <div className="container mx-auto flex  items-center justify-center gap-4 border-2 border-gray-300 rounded-md">
-      <div className="flex items-center flex-wrap">
-        <div className="flex items-center">
-          <IoLocationOutline className="text-gray-500" />
-          <input
-            type="text"
-            placeholder="Enter location"
-            className="py-2 px-4 outline-none border-gray-300 rounded-l-md "
-          />
-        </div>
-
-        <div className="flex items-center border-l-2 border-gray-300 pl-2">
+  <header className="bg-white  p-4 relative z-10  pl-56 pr-40">
+    <div className="container mx-auto flex  items-center justify-center gap-4 border-2 border-gray-300 rounded-md ">
+      <div className="flex items-center flex-wrap p-1">
+        <div className="flex items-center pl-2 w-[40vw]">
           <CiSearch className="text-gray-500" />
           <input
             type="text"
             placeholder="Search"
             className="py-2 px-4 outline-none border-gray-300 rounded-l-md "
+          />
+        </div>
+
+        <div className="flex items-center pl-2  border-l-2 border-gray-300 w-[20vw]">
+          <IoLocationOutline className="text-gray-500" />
+          <input
+            type="text"
+            placeholder="Enter location"
+            className="py-2 px-4 outline-none border-gray-300 rounded-l-md  w-full"
           />
         </div>
       </div>
@@ -47,8 +47,8 @@ const Header: React.FC = () => {
   };
 
   return (
-    <>
-      <div className="flex flex-1  w-full bg-bgCardWhite items-center content-center justify-between p-5 shadow-sm text-textNavLinkNormal max-w-screen-xl">
+    <div className="w-full flex flex-col justify-center items-center">
+      <div className="flex flex-1  w-full bg-bgCardWhite items-center content-center justify-between p-5 shadow-sm text-textNavLinkNormal max-w-screen-2xl">
         <img className="h-9" src={commonImages.company_logo} alt="logo" />
         <div className="hidden justify-between items-center uppercase ml-10 w-3/5 md:flex ">
           <Navbar />
@@ -77,7 +77,7 @@ const Header: React.FC = () => {
         </div>
       )}
       <StyledHeader />
-    </>
+    </div>
   );
 };
 
